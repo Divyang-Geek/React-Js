93bd8a11407a6d5024b56013616afb98db5fe1ff
Divyang-Geek <divyanggeek435@gmail.com>
2023-05-02 19:52:02 +0530

    setup done

---
+++ urban_crew/src/components/Header/index.js
@@ -0,0 +1,19 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <>
+            <header class="site-header">
+                <Container className="p-0">
+                    <Row>
+                        <Col lg="auto">
+
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
+        </>
+    );
+};
+
+export default Header;
