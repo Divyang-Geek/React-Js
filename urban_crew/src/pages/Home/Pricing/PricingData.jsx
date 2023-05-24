0836e4518f4ce2afd60301b6d5a7eb4f906fdd33
Divyang-Geek <divyanggeek435@gmail.com>
2023-05-24 20:32:30 +0530

    24-05-2023

---
+++ urban_crew/src/pages/Home/Pricing/PricingData.jsx
@@ -0,0 +1,19 @@
+import starterPlan from "../../../assets/images/starter-plan.svg"
+
+const PricingData = [
+    {
+        id: 1,
+        icon: starterPlan,
+        title: "Starter Plan",
+        description: "To void spirit seasons.",
+        price: {
+            currency: "$",
+            amount: 19,
+            type: "month",
+        },
+        priceServices: ["Constant desire renewal", "Two him darkness", "Given be divided days"],
+        url: "/",
+    },
+];
+
+export default PricingData;
--- urban_crew/src/pages/Home/Pricing/index.js
+++ urban_crew/src/pages/Home/Pricing/index.js
@@ -1,5 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap";
 import SecTitle from "../../../components/Common/SecTitle";
+import PricingData from "./PricingData";
+import Button from "../../../components/Common/Button";
 
 const Pricing = () => {
     return (
@@ -8,12 +10,50 @@ const Pricing = () => {
                 <div className="sec-wp">
                     <Container>
                         <Row>
-                            <Col lg={12}>
-                                <SecTitle subTitle={{ first: "Our", second: "Pricing" }}>
-                                        Choose Your <span>Pricing</span> Plan
+                            <Col lg={7} className="mx-auto">
+                                <SecTitle subTitle={{ first: "Our", second: "Pricing" }} className="mb-80 text-center white-text" h2Class="yellow-span">
+                                    Choose Your <span>Pricing</span> Plan
                                 </SecTitle>
                             </Col>
                         </Row>
+                        <div className="pricing-row">
+                            <Row className="g-xl-5 g-lg-4 g-5">
+                                {PricingData.map((data) => {
+                                    const { id, icon, title, description, price, priceServices, url } = data;
+                                    const { amount, currency, type } = price;
+                                    return (
+                                        <Col lg={4} key={id}>
+                                            <div className="pricing-box text-center">
+                                                <div className="pricing-box-img">
+                                                    <img src={icon} alt={title} />
+                                                </div>
+                                                <div className="pricing-box-head white-text">
+                                                    <h3 className="h3-title">{title}</h3>
+                                                    <p>{description}</p>
+                                                </div>
+                                                <div className="pricing-box-price">
+                                                    <span className="currency-before">{currency}</span>
+                                                    <span className="price">{amount}</span>
+                                                    <span className="period">/ {type}</span>
+                                                </div>
+                                                <div className="services-list pricing-box-content white-text">
+                                                    <ul>
+                                                        {priceServices.map((data, index) => {
+                                                            return (
+                                                                <li key={index}>
+                                                                    <span></span> {data}
+                                                                </li>
+                                                            );
+                                                        })}
+                                                    </ul>
+                                                </div>
+                                                <Button text="Purchage Now" url="/" className="yellow-btn" />
+                                            </div>
+                                        </Col>
+                                    );
+                                })}
+                            </Row>
+                        </div>
                     </Container>
                 </div>
             </div>
